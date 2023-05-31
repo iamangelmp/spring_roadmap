96ae90ba8c7c018f1a775bcf6194a4ad2632dbab
Angel Marquez <alexis.mplt@outlook.es>
2023-05-30 23:14:36 -0600

    firt commit

---
+++ 01 hello world/src/main/java/com/iamangelmp/iamangelmp/IamangelmpApplication.java
@@ -0,0 +1,13 @@
+package com.iamangelmp.iamangelmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamangelmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IamangelmpApplication.class, args);
+	}
+
+}
