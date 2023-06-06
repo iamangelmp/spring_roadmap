67bba6d712a74fb4c15edd70b040ac82b268d1a4
Angel Marquez <alexis.mplt@outlook.es>
2023-06-06 11:59:28 -0600

    first step whith Mysql connection and Sprinh Data

---
+++ com.example_SpringData/src/main/java/com/example/demo/Persona.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import lombok.Data;
+
+@Data
+public class Persona {
+	private String nombre;
+	private String apellido;
+	private String email;
+	private String cell;
+	
+}
---
+++ com.example_SpringData/src/main/java/com/example/demo/controllerInicio.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import java.util.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class controllerInicio {
+
+	@Value("${index.saludo}")
+	private String saludo;
+	
+	@GetMapping("/")
+	public String inicio(Model model) {
+		var mensaje = "Hola mundo con Thymeleaf";
+		log.info("ejecutando el controlador MVC");
+		
+		var persona = new Persona();
+		persona.setNombre("Angel");
+		persona.setApellido("Marquez");
+		persona.setEmail("alexis.mplt@gmail.com");
+		persona.setCell("5443332211");
+		
+		var persona2 = new Persona();
+		persona2.setNombre("Fernando");
+		persona2.setApellido("Lopez");
+		persona2.setEmail("email@domain.com");
+		persona2.setCell("0099887766");
+		
+		//List<Persona> personas = new ArrayList();
+		//var personas = new ArrayList();
+//		personas.add(persona);
+//		personas.add(persona2);
+
+		var personas = Arrays.asList(persona, persona2);
+		
+		model.addAttribute("mensaje", mensaje);
+		model.addAttribute("saludando", saludo);
+		model.addAttribute("personas", personas);
+		return "index";
+	}
+
+}
