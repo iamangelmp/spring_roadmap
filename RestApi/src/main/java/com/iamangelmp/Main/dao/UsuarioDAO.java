617d26e15aeea150ab23b2481055a51a74948a89
Angel Marquez <alexis.mplt@outlook.es>
2023-06-07 20:50:10 -0600

    adding mysql, lombook, hibernate and web decorators

---
+++ RestApi/src/main/java/com/iamangelmp/Main/dao/UsuarioDAO.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.iamangelmp.Main.dao;
+
+import com.iamangelmp.Main.models.Usuario;
+import java.util.List;
+
+/**
+ *
+ * @author Alexis
+ */
+public interface UsuarioDAO {
+	
+	List<Usuario> getAllUsers();
+	
+}
---
+++ RestApi/src/main/java/com/iamangelmp/Main/dao/UsuarioDAOImp.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.iamangelmp.Main.dao;
+
+import com.iamangelmp.Main.dao.UsuarioDAO;
+import com.iamangelmp.Main.models.Usuario;
+import jakarta.persistence.*;
+import java.util.List;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+/**
+ *
+ * @author Alexis
+ */
+@Repository
+@Transactional
+public class UsuarioDAOImp implements UsuarioDAO{
+	
+	@PersistenceContext
+	EntityManager entityManager;
+
+	@Override
+	@Transactional
+	public List<Usuario> getAllUsers() {
+		String query = "FROM Usuario";
+		List<Usuario> resultado = entityManager.createQuery(query).getResultList();
+		System.out.println("----------------------------------->"+resultado);
+		return resultado;
+	}
+	
+}
