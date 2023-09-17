a0d2c4c523e8c6e8b24f8e7e1c7c11ea82bba90c
Angel Marquez <alexis.mplt@outlook.es>
2023-09-17 01:19:13 -0600

    config log output in terminal

--- 01-Hello_World/src/main/java/com/imangel/Hello_World/Controller.java
+++ 01-Hello_World/src/main/java/com/imangel/Hello_World/Controller.java
@@ -1,5 +1,6 @@
 package com.imangel.Hello_World;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,12 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@Slf4j
 public class Controller {
-		
+	
 	@GetMapping("/")
 	public String start(){
-		
-		return "Hello world 90";
+		log.info("--> exucute Rest Controller");
+		log.debug("--> more info about controller");
+		return "Hello world";
 	}
 	
 }
