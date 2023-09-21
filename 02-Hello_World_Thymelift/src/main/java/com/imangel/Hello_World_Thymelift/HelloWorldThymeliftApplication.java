54e6cb263a5ec71e052c63985594f83c7ba78f42
Angel Marquez <alexis.mplt@outlook.es>
2023-09-20 23:36:50 -0600

    install to thymeleaft in spring

---
+++ 02-Hello_World_Thymelift/src/main/java/com/imangel/Hello_World_Thymelift/HelloWorldThymeliftApplication.java
@@ -0,0 +1,13 @@
+package com.imangel.Hello_World_Thymelift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldThymeliftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldThymeliftApplication.class, args);
+	}
+
+}
---
+++ 02-Hello_World_Thymelift/src/test/java/com/imangel/Hello_World_Thymelift/HelloWorldThymeliftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imangel.Hello_World_Thymelift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldThymeliftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
