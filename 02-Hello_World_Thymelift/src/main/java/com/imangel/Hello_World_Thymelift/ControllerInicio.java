8cc36138636499138b4d46ad2a83b75d6e94b4c1
Angel Marquez <alexis.mplt@outlook.es>
2023-09-21 23:56:32 -0600

    create Person class

--- 02-Hello_World_Thymelift/src/main/java/com/imangel/Hello_World_Thymelift/ControllerInicio.java
+++ 02-Hello_World_Thymelift/src/main/java/com/imangel/Hello_World_Thymelift/ControllerInicio.java
@@ -1,6 +1,8 @@
 package com.imangel.Hello_World_Thymelift;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class ControllerInicio {
 	
+	@Value("${index.saludo}")
+	private String saludo;
+	
 	@GetMapping("/")
-	public String start(){
+	public String start(Model model){
+		String msj = this.saludo;
+		model.addAttribute("msj", msj);
 		return "index";
 	}
 }
---
+++ 02-Hello_World_Thymelift/src/main/java/com/imangel/Hello_World_Thymelift/domain/Person.java
@@ -0,0 +1,16 @@
+package com.imangel.Hello_World_Thymelift.domain;
+
+/**
+ *
+ * @author Alexis
+ */
+
+public class Person {
+	protected String nombre;
+	protected String apellido;
+	protected String email;
+	protected String telefono;
+	
+	
+	
+}
