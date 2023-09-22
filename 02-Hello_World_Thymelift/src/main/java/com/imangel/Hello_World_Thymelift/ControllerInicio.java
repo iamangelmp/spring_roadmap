d8c4978f19a47906968b75a4ec99e3b3b7d36757
Angel Marquez <alexis.mplt@outlook.es>
2023-09-22 01:03:16 -0600

    display information using TH

--- 02-Hello_World_Thymelift/src/main/java/com/imangel/Hello_World_Thymelift/ControllerInicio.java
+++ 02-Hello_World_Thymelift/src/main/java/com/imangel/Hello_World_Thymelift/ControllerInicio.java
@@ -1,6 +1,9 @@
 package com.imangel.Hello_World_Thymelift;
 
 import com.imangel.Hello_World_Thymelift.domain.Person;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -27,8 +30,24 @@ public class ControllerInicio {
 		persona.setEmail("alexis.mplt@outlook.es");
 		persona.setPhone("2233445566");
 		
+		var persona2 = new Person();
+		persona2.setName("Alexis");
+		persona2.setLast_name("Peralta");
+		persona2.setEmail("alexis.mplt@gmail.com");
+		persona2.setPhone("2233445566");
+		
+		
+//		CLASSIC METHOD		
+//		List<Person> array_person = new ArrayList() ;
+//		array_person.add(persona);
+//		array_person.add(persona2);
+
+		List<Person> person_array = Arrays.asList(persona, persona2);
+		
+		
 		model.addAttribute("persona", persona);
 		model.addAttribute("msj", persona.toString());
+		model.addAttribute("persons", person_array);
 		return "index";
 	}
 }
