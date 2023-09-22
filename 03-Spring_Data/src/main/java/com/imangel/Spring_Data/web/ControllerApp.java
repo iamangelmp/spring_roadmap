5c48f262599b0f324498e92d048a92e8d8373aee
Angel Marquez <alexis.mplt@outlook.es>
2023-09-22 03:38:30 -0600

    match across view and controller

--- 03-Spring_Data/src/main/java/com/imangel/Spring_Data/web/ControllerApp.java
+++ 03-Spring_Data/src/main/java/com/imangel/Spring_Data/web/ControllerApp.java
@@ -1,9 +1,11 @@
-package com.imangel.Spring_Data;
+package com.imangel.Spring_Data.web;
 
+import com.imangel.Spring_Data.DAO.PersonDAO;
 import com.imangel.Spring_Data.Domain.Person;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,29 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Controller
 public class ControllerApp {
+	
+	@Autowired
+	private PersonDAO personDAO;
 
 	@GetMapping("/")
 	public String start(Model model) {
 		String saludo = "hi from controller :D";
 		
-		Person persona = new Person();
-		persona.setName("Angel");
-		persona.setLast_name("Marquez");
-		persona.setEmail("alexis.mplt@outlook.es");
-		persona.setPhone("5544663377");
+		var personas = personDAO.findAll();
 		
-		
-		Person persona2 = new Person();
-		persona2.setName("Alexis");
-		persona2.setLast_name("Peralta");
-		persona2.setEmail("alexis.mplt@gmail.com");
-		persona2.setPhone("9988776655");
-		
-		List<Person> array_person = Arrays.asList(persona, persona2);
-		
-		
-		
-		model.addAttribute("persons", array_person);
+		model.addAttribute("persons", personas);
 		model.addAttribute("saludo", saludo);
 		return "index";
 	}
