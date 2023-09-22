41cf5e6fdf2f5bacbcbf3d5d3572abb0d7ba813d
Angel Marquez <alexis.mplt@outlook.es>
2023-09-22 00:32:54 -0600

    use lombok for Person Class and display data from view

--- 02-Hello_World_Thymelift/src/main/java/com/imangel/Hello_World_Thymelift/ControllerInicio.java
+++ 02-Hello_World_Thymelift/src/main/java/com/imangel/Hello_World_Thymelift/ControllerInicio.java
@@ -1,5 +1,6 @@
 package com.imangel.Hello_World_Thymelift;
 
+import com.imangel.Hello_World_Thymelift.domain.Person;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,7 +20,15 @@ public class ControllerInicio {
 	@GetMapping("/")
 	public String start(Model model){
 		String msj = this.saludo;
-		model.addAttribute("msj", msj);
+		
+		Person persona = new Person();
+		persona.setName("Angel");
+		persona.setLast_name("Marquez");
+		persona.setEmail("alexis.mplt@outlook.es");
+		persona.setPhone("2233445566");
+		
+		model.addAttribute("persona", persona);
+		model.addAttribute("msj", persona.toString());
 		return "index";
 	}
 }
--- 02-Hello_World_Thymelift/src/main/java/com/imangel/Hello_World_Thymelift/domain/Person.java
+++ 02-Hello_World_Thymelift/src/main/java/com/imangel/Hello_World_Thymelift/domain/Person.java
@@ -1,16 +1,16 @@
 package com.imangel.Hello_World_Thymelift.domain;
 
+import lombok.Data;
+
 /**
  *
  * @author Alexis
  */
 
+@Data
 public class Person {
-	protected String nombre;
-	protected String apellido;
+	protected String name;
+	protected String last_name;
 	protected String email;
-	protected String telefono;
-	
-	
-	
+	protected String phone;	
 }
