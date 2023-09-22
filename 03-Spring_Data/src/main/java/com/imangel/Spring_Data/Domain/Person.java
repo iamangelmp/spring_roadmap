0f25d7bc1cda652dd7ec3d496ff6320dda59ba0d
Angel Marquez <alexis.mplt@outlook.es>
2023-09-22 03:35:00 -0600

    define the schema to read and write from spring and bd

--- 03-Spring_Data/src/main/java/com/imangel/Spring_Data/Domain/Person.java
+++ 03-Spring_Data/src/main/java/com/imangel/Spring_Data/Domain/Person.java
@@ -1,5 +1,11 @@
 package com.imangel.Spring_Data.Domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 /**
@@ -7,10 +13,27 @@ import lombok.Data;
  * @author Alexis
  */
 @Data
+@Entity
+@Table(name="person")
 public class Person {
-	protected String name;
-	protected String last_name;
-	protected String email;
-	protected String phone;
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
+	protected Long user_id;
+	
+	@Column(name = "user_name")
+	protected String user_name;
+	
+	@Column(name = "user_last_name")
+	protected String user_last_name;
+	
+	@Column(name = "user_email")
+	protected String user_email;
+	
+	@Column(name = "user_phone")
+	protected String user_phone;
 	
 }
