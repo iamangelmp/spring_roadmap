90c0242acce8e811c19db35b9e631cc7ffce4ff5
Angel Marquez <alexis.mplt@outlook.es>
2023-09-23 23:59:06 -0600

    create bussiness layer to connect from DB

--- 03-Spring_Data/src/main/java/com/imangel/Spring_Data/service/PersonServiceImpl.java
+++ 03-Spring_Data/src/main/java/com/imangel/Spring_Data/service/PersonServiceImpl.java
@@ -1,32 +1,48 @@
 package com.imangel.Spring_Data.service;
 
+import com.imangel.Spring_Data.DAO.PersonDAO;
 import com.imangel.Spring_Data.Domain.Person;
+import java.util.ArrayList;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
  * @author Alexis
  */
-public class PersonServiceImpl implements PersonService{
+@Service
+public class PersonServiceImpl implements PersonService {
+
+	@Autowired
+	private PersonDAO personaDao;
 
 	@Override
+	@Transactional(readOnly = true)
 	public List<Person> ListPersons() {
-		
+		return (List<Person>) personaDao.findAll();
 	}
 
 	@Override
+	@Transactional
 	public void savePerson(Person person) {
-		
+		personaDao.save(person);
 	}
 
 	@Override
+	@Transactional
 	public void deletePerson(Person person) {
-		
+		personaDao.delete(person);
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public Person searchPerson(Person person) {
-		
+		List<Long> userIds = new ArrayList<>();
+		userIds.add(person.getUser_id());
+		return (Person) personaDao.findAllById(userIds);
+
 	}
-	
+
 }
--- 03-Spring_Data/src/main/java/com/imangel/Spring_Data/web/ControllerApp.java
+++ 03-Spring_Data/src/main/java/com/imangel/Spring_Data/web/ControllerApp.java
@@ -1,15 +1,11 @@
 package com.imangel.Spring_Data.web;
 
 import com.imangel.Spring_Data.DAO.PersonDAO;
-import com.imangel.Spring_Data.Domain.Person;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import com.imangel.Spring_Data.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
@@ -19,13 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class ControllerApp {
 	
 	@Autowired
-	private PersonDAO personDAO;
+	private PersonService personDAO;
 
 	@GetMapping("/")
 	public String start(Model model) {
 		String saludo = "hi from controller :D";
 		
-		var personas = personDAO.findAll();
+		var personas = personDAO.ListPersons();
 		
 		model.addAttribute("persons", personas);
 		model.addAttribute("saludo", saludo);
