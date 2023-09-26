96ef6820c34e649a1bad0888624c6bc07f11ee0d
Angel Marquez <alexis.mplt@outlook.es>
2023-09-25 23:12:39 -0600

    adding auto increment propiertie

--- 04-CRUD/src/main/java/com/imangel/CRUD/domain/Person.java
+++ 04-CRUD/src/main/java/com/imangel/CRUD/domain/Person.java
@@ -2,6 +2,8 @@ package com.imangel.CRUD.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -16,6 +18,7 @@ import lombok.Data;
 public class Person {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     protected Long user_id;
 
