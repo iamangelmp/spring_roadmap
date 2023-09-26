9f07eb075c8e88c5cb45103dc9ec45d42edb2bb0
Angel Marquez <alexis.mplt@outlook.es>
2023-09-25 23:15:01 -0600

    define route for create a new user and routes for navigate

--- 04-CRUD/src/main/java/com/imangel/CRUD/web/ControllerApp.java
+++ 04-CRUD/src/main/java/com/imangel/CRUD/web/ControllerApp.java
@@ -1,11 +1,13 @@
 package com.imangel.CRUD.web;
 
 import com.imangel.CRUD.DAO.PersonDAO;
+import com.imangel.CRUD.domain.Person;
 import com.imangel.CRUD.service.ServicePerson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  *
@@ -14,18 +16,29 @@ import org.springframework.ui.Model;
 @Controller
 public class ControllerApp {
 
-    @Autowired
-    private ServicePerson service;
+	@Autowired
+	private ServicePerson service;
 
-    @GetMapping("/")
-    public String getData(Model model) {
-        
-        var persons = service.getAllPesons();
+	@GetMapping("/")
+	public String getData(Model model) {
 
-        String saludo = "Hola desde el controlador D:";
-        model.addAttribute("persons", persons);
-        model.addAttribute("saludo", saludo);
-        return "index";
-    }
+		var persons = service.getAllPesons();
+
+		String saludo = "Hi from controller :D";
+		model.addAttribute("persons", persons);
+		model.addAttribute("saludo", saludo);
+		return "index";
+	}
+
+	@GetMapping("/add")
+	public String create(Person person) {
+		return "add";
+	}
+
+	@PostMapping("/save")
+	public String save(Person person) {
+		service.createPerson(person);
+		return "redirect:/";
+	}
 
 }
